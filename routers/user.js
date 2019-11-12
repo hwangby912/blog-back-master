963921a9f863b03f9d470a723bd578c80ce46a63
hwangby912 <quddbs912@naver.com>
2019-11-12 15:04:33 +0900

    complete login

--- routers/user.js
+++ routers/user.js
@@ -3,6 +3,7 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const router = express.Router();
 const { User, validateUser } = require("../models/user");
+const { jwtSecret } = require("../common/jwt_config");
 
 router.post("/join", async (req, res, next) => {
   const { name, email, password } = req.body;
@@ -18,6 +19,35 @@ router.post("/join", async (req, res, next) => {
   const saveResult = await user.save();
   res.json({ result: true });
   next();
+});
+
+router.post("/login", async (req, res, next) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email: email });
+  if (!user) {
+    res.json({ result: false });
+    next();
+    return;
+  }
+  const result = await bcrypt.compare(password, user.password);
+  // result가 Promise<true>이면 token을 만들어 주자
+  if (result) {
+    const token = jwt.sign(
+      {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        admin: user.admin
+      },
+      jwtSecret,
+      { expiresIn: "1m" }
+    );
+    res.json({ result: true, token, admin: user.admin });
+    next();
+  } else {
+    res.json({ result: false });
+    next();
+  }
 });
 
 module.exports = router;
