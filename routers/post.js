011d753c43436310ff283fd8d1bd2e66a79dea19
hwangby912 <quddbs912@naver.com>
2019-11-13 14:27:46 +0900

    fix bug

--- routers/post.js
+++ routers/post.js
@@ -3,31 +3,41 @@ const router = express.Router();
 const auth = require("../common/auth")();
 const { Post, validatePost } = require("../models/post");
 const { Tag } = require("../models/tag");
+const wrapper = require("../common/wrapper");
 
-router.post("/", auth.authenticate(), async (req, res, next) => {
-  const { title, contents, tags } = req.body;
-  if (validatePost(req.body).error) {
-    res.status(400).json({ result: false, error: "Not fit on the form" });
+router.post(
+  "/",
+  auth.authenticate(),
+  wrapper(async (req, res, next) => {
+    if (!req.user.admin) {
+      res.json({ error: "unauthorized" });
+      next();
+      return;
+    }
+    const { title, contents, tags } = req.body;
+    if (validatePost(req.body).error) {
+      res.status(400).json({ result: false, error: "Not fit on the form" });
+      next();
+      return;
+    }
+    // post 작성
+    const post = new Post({
+      title,
+      author: req.user.id,
+      contents,
+      tags
+    });
+    await post.save();
+    // tag에 update
+    for (const tag_id of tags) {
+      // tag = { _id, name, posts }가 있을 것임
+      const tag = await Tag.findById(tag_id);
+      tag.posts.push(post._id);
+      await tag.save();
+    }
+    res.json({ result: true });
     next();
-    return;
-  }
-  // post 작성
-  const post = new Post({
-    title,
-    author: req.user.id,
-    contents,
-    tags
-  });
-  await post.save();
-  // tag에 update
-  for (const tag_id of tags) {
-    // tag = { _id, name, posts }가 있을 것임
-    const tag = await Tag.findById(tag_id);
-    tag.posts.push(post._id);
-    await tag.save();
-  }
-  res.json({ result: true });
-  next();
-});
+  })
+);
 
 module.exports = router;
--- routers/user.js
+++ routers/user.js
@@ -4,61 +4,71 @@ const bcrypt = require("bcrypt");
 const router = express.Router();
 const { User, validateUser } = require("../models/user");
 const { jwtSecret } = require("../common/jwt_config");
+const wrapper = require("../common/wrapper");
 
-router.post("/join", async (req, res, next) => {
-  const { name, email, password } = req.body;
-  if (validateUser(req.body).error) {
-    // 인증을 통과 못한 경우
-    res.status(400).json({ result: false });
-    next();
-    return;
-  }
-  const saltRound = 10;
-  const hashedPW = await bcrypt.hash(password, saltRound);
-  const user = new User({ name, email, password: hashedPW });
-  const saveResult = await user.save();
-  res.json({ result: true });
-  next();
-});
-
-router.post("/login", async (req, res, next) => {
-  const { email, password } = req.body;
-  const user = await User.findOne({ email: email });
-  if (!user) {
-    res.json({ result: false });
-    next();
-    return;
-  }
-  const result = await bcrypt.compare(password, user.password);
-  // result가 Promise<true>이면 token을 만들어 주자
-  if (result) {
-    const token = jwt.sign(
-      {
-        id: user._id,
-        name: user.name,
-        email: user.email,
-        admin: user.admin
-      },
-      jwtSecret,
-      { expiresIn: "1h" }
-    );
-    res.json({ result: true, token, admin: user.admin });
-    next();
-  } else {
-    res.json({ result: false });
-    next();
-  }
-});
-
-router.get("/email", async (req, res, next) => {
-  const email = req.query.email;
-  const user = await User.findOne({ email });
-  if (user) {
-    res.json({ result: false });
-  } else {
+router.post(
+  "/join",
+  wrapper(async (req, res, next) => {
+    const { name, email, password } = req.body;
+    if (validateUser(req.body).error) {
+      // 인증을 통과 못한 경우
+      res.status(400).json({ result: false });
+      next();
+      return;
+    }
+    const saltRound = 10;
+    const hashedPW = await bcrypt.hash(password, saltRound);
+    const user = new User({ name, email, password: hashedPW });
+    const saveResult = await user.save();
     res.json({ result: true });
-  }
-  next();
-});
+    next();
+  })
+);
+
+router.post(
+  "/login",
+  wrapper(async (req, res, next) => {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email: email });
+    if (!user) {
+      res.json({ result: false });
+      next();
+      return;
+    }
+    const result = await bcrypt.compare(password, user.password);
+    // result가 Promise<true>이면 token을 만들어 주자
+    if (result) {
+      const token = jwt.sign(
+        {
+          id: user._id,
+          name: user.name,
+          email: user.email,
+          admin: user.admin
+        },
+        jwtSecret,
+        { expiresIn: "1h" }
+      );
+      res.json({ result: true, token, admin: user.admin });
+      next();
+    } else {
+      res.json({ result: false });
+      next();
+    }
+  })
+);
+
+router.get(
+  "/email",
+  wrapper(async (req, res, next) => {
+    const email = req.query.email;
+    const user = await User.findOne({ email });
+    if (user) {
+      res.json({ result: false });
+    } else {
+      res.json({ result: true });
+    }
+    next();
+  })
+);
 
 module.exports = router;
